6ea59f82e87ed6c4542aa052b0e91c79072a64d4
David Coomer <dmcoomer@hotmail.com>
2022-03-22 16:22:40 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+// Write your solution here!    
+ const cats = ["Milo", "Otis", "Garfield"];
+
+ 
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    return name 
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    return name 
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+    return name
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+    return name
+}
+
+ function prependCat(){
+
+    const newArr = ["Arnold", "Milo", "Otis", "Garfield"];
+    return newArr
+ }
+
+ function appendCat(){
+    const newArr = [  "Milo", "Otis", "Garfield", "Broom"]
+    return newArr
+ }
+
+ function removeLastCat(){
+     const newArr = ["Milo", "Otis",]
+     return newArr
+ }
+
+ function removeFirstCat(){
+     const newArr = ["Otis", "Garfield"]
+     return newArr
+ }
